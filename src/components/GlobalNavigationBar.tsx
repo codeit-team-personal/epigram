fa0ec5cd9d336064033188478265919ae95afbee
codesmoo <52499305+codesmoo@users.noreply.github.com>
2025-08-26 18:43:29 +0900

    fix GNB markup

--- src/components/GlobalNavigationBar.tsx
+++ src/components/GlobalNavigationBar.tsx
@@ -3,17 +3,20 @@ import { User, Search } from 'lucide-react';
 
 export default function GlobalNavigationBar() {
   return (
-    <div className='max-w-[1920px] h-[80px] border flex items-center'>
-      <div className='flex border px-20 justify-between items-center grow'>
+    <div className='max-w-[1920px] lg:h-[80px] md:h-[60px] h-[52px] border flex items-center'>
+      <div className='flex border lg:px-20 md:px-15 px-5 justify-between items-center grow'>
         <span>
-          <Search size={24} />
+          <Search className='lg:size-9 size-5' />
         </span>
-        <div className='flex gap-3'>
-          <Image src={'/images/logo.png'} width={36} height={36} alt='logo' />
-          <span className='text-2xl'>Epigram</span>
+        <div className='flex gap-3 items-center'>
+          <div className='relative lg:size-9 size-6'>
+            <Image src={'/images/logo.png'} fill alt='logo' />
+          </div>
+
+          <span className='lg:text-2xl text-xl'>Epigram</span>
         </div>
         <span>
-          <User size={24} />
+          <User className='lg:size-9 size-5' />
         </span>
       </div>
     </div>
