e8a90815776868523e3df709b053b3b6b93902c6
codesmoo <52499305+codesmoo@users.noreply.github.com>
2025-09-04 10:18:48 +0900

    [송성학] fix: 패스워드 확인시 재검증로직 추가

--- src/hooks/useRegisterForm.ts
+++ src/hooks/useRegisterForm.ts
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { registerSchema, RegisterSchema } from "@/schemas/signupSchema";
+import { useEffect } from "react";
 
 export function useRegisterForm() {
-  return useForm<RegisterSchema>({
+  const form = useForm<RegisterSchema>({
     resolver: zodResolver(registerSchema),
     defaultValues: {
       email: "",
@@ -13,4 +14,16 @@ export function useRegisterForm() {
     },
     mode: "onChange", // 실시간 validation
   });
+
+  const password = form.watch("password");
+  const confirmTouched = form.formState.touchedFields.confirmPassword;
+
+  useEffect(() => {
+    if (confirmTouched) {
+      // password가 바뀔 때 confirmPassword도 같이 재검증
+      form.trigger("confirmPassword");
+    }
+  }, [password, confirmTouched, form]);
+
+  return form;
 }
