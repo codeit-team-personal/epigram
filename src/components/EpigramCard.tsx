8d8ac683a4673449feb8b0373b8c42098d3290ac
codesmoo <52499305+codesmoo@users.noreply.github.com>
2025-09-10 19:20:57 +0900

    [송성학] design: feed 페이지 마크업

--- src/components/EpigramCard.tsx
+++ src/components/EpigramCard.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { Epigram } from '@/types/today';
+import { Epigram } from "@/types/today";
 
 export default function EpigramCard({ data }: { data: Epigram }) {
   return (
-    <div className='font-iropke text-2xl'>
-      <div className='p-6 rounded-xl shadow-sm border border-line-100 bg-card relative overflow-hidden'>
+    <div className='font-iropke lg:text-2xl md:text-base text-sm text-black-600 lg:w-[585px] lg:h-[307px] md:w-[294px] md:h-[214px] w-[312px] h-[172px]'>
+      <div className='p-6 rounded-xl shadow-sm border border-line-100 bg-card relative overflow-hidden '>
         {/* 줄무늬 배경 */}
         <div
           className='absolute inset-0 bg-[repeating-linear-gradient(to_bottom,white_0px,white_24px,#f2f2f2_25px)]'
           aria-hidden='true'
         />
         {/* 콘텐츠 */}
-        <div className='relative'>
+        <div className='relative flex flex-col justify-between lg:h-[211px] md:h-[132px] h-[92px]'>
           {/* 본문 */}
           <p className='leading-relaxed'>{data.content}</p>
 
